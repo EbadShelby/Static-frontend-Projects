a709f074a9997b883cd4caf2e3f955c055268a2d
EbadShelby <ebadyasser123xd@gmail.com>
2025-10-19 07:17:08 +0800

    simple faq: done project

---
+++ simple-FAQ/src/App.vue
@@ -0,0 +1,53 @@
+<script setup></script>
+
+<template>
+  <header class="py-20 text-center motion-preset-seesaw-sm motion-preset-blur-down delay-300">
+    <h1 class="text-4xl font-bold">Frequently Asked Questions</h1>
+    <p>Browse through most frequently asked questions</p>
+  </header>
+  <main class="bg-[#D2D5DA20]  py-20">
+    <section
+      class="mx-auto max-w-150 space-y-5 border-b-2 border-[#D2D5DA] pb-10 motion-preset-blur-down motion-delay-500"
+    >
+      <h2 class="text-2xl font-bold">How can i track my order?</h2>
+      <p>You can track your order using the following steps:</p>
+      <ol class="list-decimal pl-8 space-y-2">
+        <li class="motion-preset-slide-right motion-delay-700">Go to the Order <a href="#">Tracking page</a>.</li>
+        <li class="motion-preset-slide-right motion-delay-900">Enter your order number and email address.</li>
+        <li class="motion-preset-slide-right motion-delay-1100">
+          Click on the Track Order button to view the current status of your
+          shipment.
+        </li>
+      </ol>
+      <p>
+        If you encounter any issues, please visit our
+        <a href="#">Help Center</a>.
+      </p>
+    </section>
+
+    <section class="mx-auto max-w-150 space-y-5 pt-10 motion-preset-blur-down motion-delay-1500">
+      <h2 class="text-2xl font-bold">What is your return policy?</h2>
+      <p>
+        We offer a 30-day return policy on most items. Here are some key points:
+      </p>
+      <ul class="list-disc space-y-2 pl-8">
+        <li class="motion-preset-slide-right motion-delay-1800">
+          <span class="font-bold">Items must be in original condition</span>:
+          Unworn, unused, and unwashed.
+        </li>
+        <li class="motion-preset-slide-right motion-delay-2000">
+          <span class="font-bold">Include original packaging and tags</span>:
+          All items should be returned with their original packaging and tags
+        </li>
+        <li class="motion-preset-slide-right motion-delay-2200">
+          <span class="font-bold">Proof of purchase</span>: A receipt or proof
+          of purchase is required.
+        </li>
+      </ul>
+      <p>
+        For more detailed information, read our full
+        <a href="#">Return Policy</a>.
+      </p>
+    </section>
+  </main>
+</template>
